7b75ea74154d84b42238179f1b723ffb7d8fcbc8
Johan Selin <johanselin99@gmail.com>
2021-05-06 15:08:00 +0200

    Added pictures for buttons and a grid to be aligned

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,22 +1,25 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("base.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 1600, 900));
         primaryStage.show();
+        
+        
     }
-
-
+    
     public static void main(String[] args) {
         launch(args);
     }
