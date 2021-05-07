04740646131e31ec989370a8e9229e41d3c91f93
Johan Selin <johanselin99@gmail.com>
2021-05-07 15:42:48 +0200

    cleaned up

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,23 +1,26 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+import java.util.ResourceBundle;
 
+public class Main extends Application {
+    
+    
+    //   /../resources/.ttf
     @Override
     public void start(Stage primaryStage) throws Exception{
+        //ResourceBundle bundle = java.util.ResourceBundle.getBundle("sample/resources/Main");
         Parent root = FXMLLoader.load(getClass().getResource("base.fxml"));
+        
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 1600, 900));
         primaryStage.show();
-        
-        
     }
     
     public static void main(String[] args) {
