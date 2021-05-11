bd8c505dcbd591e0ea9b49317f6583d59c34b569
Johan Selin <johanselin99@gmail.com>
2021-05-11 19:39:45 +0200

    Placeholder methods for testing cart functionality added

--- src/sample/UIController.java
+++ src/sample/UIController.java
@@ -4,11 +4,10 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.VBox;
 
 
 import java.net.URL;
@@ -21,13 +20,30 @@ public class UIController implements Initializable {
     @FXML public Button shoppingButton, historyButton, myPagesButton, helpButton;
     @FXML private Label iMat;
     @FXML private ImageView testimage;
+    @FXML private VBox shoppingCart;
+    @FXML private FlowPane shoppingCartPane;
     
     private TopMenuBarButtons topMenuBarButtons;
     
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         initMenuButtons();
+        
+        addPlaceholderCartItems();
     }
+    
+    /**
+     * Placeholder method which just adds fake items in the shopping cart. Gives a bunch of processValue errors
+     * (atleast for me) shouldn't do any harm though
+     */
+    private void addPlaceholderCartItems() {
+        shoppingCartPane.getChildren().clear(); // Have to do this even if it's empty at launch for unknown reason
+        for (int i = 0; i < 16; i++) {
+            shoppingCartPane.getChildren().add(new CartItem(this, i + "x Test item"));
+        }
+    }
+    
+    
     
     /**
      * Initializes buttons to be "toggled" by setting their id's (has to be the same as their css id
