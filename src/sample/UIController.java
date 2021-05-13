483cb926513f3c59002144d3ac95da09667f5fba
Johan Selin <johanselin99@gmail.com>
2021-05-14 01:13:41 +0200

    Expanding cart possible, sliding animation added

--- src/sample/UIController.java
+++ src/sample/UIController.java
@@ -1,5 +1,6 @@
 package sample;
 
+import javafx.animation.AnimationTimer;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -9,6 +10,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Line;
 
 
 import java.net.URL;
@@ -19,24 +21,55 @@ public class UIController implements Initializable {
     public BackendController backend = new BackendController();
     public MyPagesController myPagesController = new MyPagesController(this);
     public HistoryController historyController = new HistoryController(this);
+    private AnimationTimer timer = new MyTimer();
     
-    @FXML public Button shoppingButton, historyButton, myPagesButton, helpButton;
+    @FXML public Button shoppingButton, historyButton, myPagesButton, helpButton, expandButton;
     @FXML private Label iMat;
     @FXML private ImageView testimage;
     @FXML private VBox shoppingCart;
     @FXML private FlowPane shoppingCartPane;
-    @FXML public AnchorPane sideMenuParentAnchorPane, parentView;
+    @FXML public AnchorPane sideMenuParentAnchorPane, parentView, shoppingCartAnchorPane;
+    @FXML private Line cartLineDivider;
+    @FXML private ScrollPane shoppingCartScrollPane;
     
     private TopMenuBarButtons topMenuBarButtons;
     private SideMenus sideMenus = new SideMenus(this);
+    private boolean shoppingCartExpanded;
 
     
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        shoppingCartExpanded = false;
         initMenuButtons();
         addPlaceholderCartItems();
         addSideMenus();
         addMainViews();
+        shoppingCartScrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+    }
+    
+    
+    /**
+     * Method is called when expanding shopping cart.
+     * Resizes necessary elements and changes the button-text.
+     */
+    public void expandShoppingCart() {
+        if (shoppingCartAnchorPane.getMaxWidth() < 500) {
+            shoppingCartExpanded = false;
+            //shoppingCartAnchorPane.setMaxWidth(500); uncomment for instant expansion
+            timer.start();
+            cartLineDivider.setEndX(cartLineDivider.getEndX() + 225);
+            expandButton.setText("Minska varukorg");
+        }
+        else {
+            shoppingCartExpanded = true;
+            //shoppingCartAnchorPane.setMaxWidth(275);
+            timer.start();
+            cartLineDivider.setEndX(245);
+            expandButton.setText("Ändra i varukorgen");
+        }
+        for (Node item : shoppingCartPane.getChildren()) {
+            ((CartItem) item).resizeNameLabel();
+        }
     }
     
     private void addMainViews() {
@@ -56,8 +89,9 @@ public class UIController implements Initializable {
      */
     private void addPlaceholderCartItems() {
         shoppingCartPane.getChildren().clear(); // Have to do this even if it's empty at launch for unknown reason
+        shoppingCartPane.getChildren().add(new CartItem(this, "1x Långt ooooooooord"));
         for (int i = 0; i < 16; i++) {
-            shoppingCartPane.getChildren().add(new CartItem(this, i + "x Test item"));
+            shoppingCartPane.getChildren().add(new CartItem(this, i + "x Test Item"));
         }
     }
     
@@ -92,5 +126,55 @@ public class UIController implements Initializable {
         sideMenus.changeSideMenu(id);
     }
     
+    /**
+     * Animation timer for sliding out the cart when expanded.
+     */
+    private class MyTimer extends AnimationTimer {
     
+        double speed = 20;
+        
+        @Override
+        public void handle(long l) {
+            slideOut();
+        }
+    
+        /**
+         * Method is called every frame, starts with high speed then slows down at the end
+         */
+        private void slideOut() {
+            if (!shoppingCartExpanded) {
+                if (speed >= 3) {
+                    speed -= 1;
+                }
+    
+                if (shoppingCartAnchorPane.getMaxWidth() > 485) {
+                    speed = 1;
+                }
+                shoppingCartAnchorPane.setMaxWidth(shoppingCartAnchorPane.getMaxWidth() + speed);
+    
+                if (shoppingCartAnchorPane.getMaxWidth() >= 500) {
+                    stop();
+                    speed = 20;
+                    shoppingCartAnchorPane.setMaxWidth(500);
+                }
+            }
+            else {
+                if (speed >= 3) {
+                    speed -= 1;
+                }
+    
+                if (shoppingCartAnchorPane.getMaxWidth() < 290) {
+                    speed = 1;
+                }
+                shoppingCartAnchorPane.setMaxWidth(shoppingCartAnchorPane.getMaxWidth() - speed);
+                
+    
+                if (shoppingCartAnchorPane.getMaxWidth() <= 275) {
+                    stop();
+                    speed = 20;
+                    shoppingCartAnchorPane.setMaxWidth(275);
+                }
+            }
+        }
+    }
 }
